a1c16ae6f2c5d5088d7a9ac16cf5e363a9766c08
Daniel Ancines <daniel.ancines@gmail.com>
2025-06-16 13:50:48 -0300

    Example project

---
+++ src/maui-webview-interop-issue/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace maui_webview_interop_issue
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenWindowButton_Clicked(object sender, EventArgs e)
+        {
+            var window = new Window(new ContentPage() { Content = new WebView() { Source = "https://microsoft.com" } });
+            Application.Current?.OpenWindow(window);
+        }
+
+        private void OpenChildWindowButton_Clicked(object sender, EventArgs e)
+        {
+            var window = new Window(new ContentPage() { Content = new WebView() { Source = "https://microsoft.com" } });
+            var mainWindowHandle = (Application.Current?.MainPage.Window.Handler.PlatformView as MauiWinUIWindow).WindowHandle;
+
+            Application.Current?.OpenWindow(window);
+            var windowHandle = (window.Handler.PlatformView as MauiWinUIWindow).WindowHandle;
+
+            NativeInterop.SetParent(windowHandle, mainWindowHandle);
+        }
+    }
+
+    public static class NativeInterop
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+    }
+}
